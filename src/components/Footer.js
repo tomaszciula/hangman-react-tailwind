8031371296c9c90c49f0ceab8794bc46aeafa0cb
tomaszciula <tomciuns@gmail.com>
2021-12-16 19:18:46 +0100

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="bg-black text-4xl text-white flex justify-center items-center p-12">
+      Footer
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = ({ sentence }) => {
+  return (
+    <div className="bg-black text-white p-12 flex justify-center text-4xl ">
+      {sentence}
+    </div>
+  );
+};
+
+export default Header;
